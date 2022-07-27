99940ce088b86206961ca33b669d4e8a5e8316b6
HushNet <pakhomov.alx@mail.ru>
2022-07-27 14:55:10 +0300

    FinalTestDir

--- VoiceTexterBot/Extensions/DirectoryExtension.cs
+++ VoiceTexterBot/Extensions/DirectoryExtension.cs
@@ -11,13 +11,7 @@ public class DirectoryExtension
     public static string GetSolutionRoot()
     {
         var dir = Path.GetDirectoryName(Directory.GetCurrentDirectory());
-        Console.WriteLine(dir);
-        var fullname = Directory.GetParent(dir).FullName;
-        Console.WriteLine(fullname);
-        var projectRoot = fullname.Substring(0, fullname.Length - 4);
-        Console.WriteLine(projectRoot);
-
-        Console.WriteLine(Directory.GetParent(projectRoot)?.FullName);
-        return Directory.GetParent(projectRoot)?.FullName;
+        
+        return Directory.GetParent(dir)?.FullName;
     }
 }
